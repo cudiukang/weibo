3a883a1f20482f1a741bd70be367abc0f346da71
cuidukang <56304567@qq.com>
2019-12-26 12:41:37 +0800

    seconde

--- weibo/src/main/java/com/cdk/controller/TestController.java
+++ weibo/src/main/java/com/cdk/controller/TestController.java
@@ -2,10 +2,11 @@ package com.cdk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-	
+	@RequestMapping("test")
 	public String test(String name,Model model) {
 		model.addAttribute("name", name);
 		return "test";
